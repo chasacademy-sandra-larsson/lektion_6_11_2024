051ca0689b5f9927f3f4f33b60c035880f275a54
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-11-06 10:59:58 +0100

    added lesson content

--- fetch-todos/fetch.js
+++ fetch-todos/fetch.js
@@ -3,8 +3,51 @@ const ul = document.getElementById("todos");
 //Alternativ 1. Använder async / await för att hämta data från JSON filen. ES2017
 
 
+async function getTodos() {
+  try {
+  const response = await fetch("./todos.json");
+  if(!response.ok) {
+    throw new Error(`HTTP ERROR STATUS ${response.status}`);
+  }
+
+  const data = await response.json();
+   displayTodos(data.todos);
+
+console.log("INNE I ASYNC FUNKTION SNART KLAR!!!")
+
+    } catch(error) {
+        console.error("Error", error)
+    }
+}
+
+
+function displayTodos(todos) {
+
+  todos.forEach(function(todo) {
+    const li = document.createElement("li");
+    li.textContent = todo.text;
+    ul.appendChild(li);
+
+  })
+
+}
+
+
+
+
+
 // Alternativ 2. Använder .then() för att hämta data från JSON filen. Kallas även Promise Chaining. ES2015
 
 
 // Visa att funktion getTodos är async och inte sync. Synkrona körs efter varandra, i taget
+getTodos();
+console.log("Ett");
+console.log("Två");
+console.log("tre");
+for(let i = 0; i < 10; i++) {
+  console.log(i);
+}
+console.log("fyra");
+console.log("fem");
+
 

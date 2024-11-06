f2780ae2fee80c143ec92c3219d6e4ef53d3f733
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-11-06 11:31:18 +0100

    finished fetch media

--- fetch-media/fetch.js
+++ fetch-media/fetch.js
@@ -4,12 +4,50 @@ const booksList = document.getElementById('books');
 
 
 // Asynkron funktion som hämtar media-data
+async function getMedia() {
+    try {
+        const response = await fetch("./media.json");
 
+        if(!response.ok) {
+            throw new Error("HTTP" + response.status);
+        }
+        const data = await response.json();
+        return data;
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+// En async funktion returnerar en Promise - vi ska prata mer om det i nästa kurs!
+// const data = getMedia();
+// console.log(data)
 
 
 // Visa filmer i html
+async function displayMovies() {
+    const data = await getMedia();
+    data.movies.forEach(movie => {
+        const li = document.createElement("li");
+        li.textContent = movie.title;
+        moviesList.appendChild(li)
+    });
+    
 
+}
+displayMovies();
 
 // Visa böcker i html
+async function displayBooks() {
+    const data = await getMedia();
+    data.books.forEach((book) => {
+        const li = document.createElement("li");
+        li.textContent = book.title;
+        booksList.appendChild(li)
+    });
+    
+
+}
+displayBooks();
 
 
--- fetch-todos/fetch.js
+++ fetch-todos/fetch.js
@@ -1,42 +1,47 @@
 const ul = document.getElementById("todos");
 
-//Alternativ 1. Använder async / await för att hämta data från JSON filen. ES2017
-
-
+// Alternativ 1. Använder async / await för att hämta data från JSON filen. ES2017
+// Använd gärna denna som mall när ni använder fetch!
 async function getTodos() {
   try {
-  const response = await fetch("./todos.json");
-  if(!response.ok) {
-    throw new Error(`HTTP ERROR STATUS ${response.status}`);
-  }
-
-  const data = await response.json();
-   displayTodos(data.todos);
-
-console.log("INNE I ASYNC FUNKTION SNART KLAR!!!")
-
-    } catch(error) {
-        console.error("Error", error)
+    const response = await fetch("./todos.json");
+    if (!response.ok) {
+      throw new Error(`HTTP ERROR STATUS ${response.status}`);
     }
+    const data = await response.json();
+    displayTodos(data.todos);
+    console.log("INNE I ASYNC FUNKTION SNART KLAR!!!");
+
+  } catch (error) {
+    console.error("Error", error);
+  }
 }
 
-
 function displayTodos(todos) {
-
-  todos.forEach(function(todo) {
+  todos.forEach(function (todo) {
     const li = document.createElement("li");
     li.textContent = todo.text;
     ul.appendChild(li);
-
-  })
-
+  });
 }
 
-
-
-
-
 // Alternativ 2. Använder .then() för att hämta data från JSON filen. Kallas även Promise Chaining. ES2015
+// function getTodos() {
+//   fetch("./todos.json")
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(`HTTP ERROR status: ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log("fetching todos");
+//       displayTodos(data.todos);
+//     })
+//     .catch((error) => {
+//       console.error("Error", error);
+//     });
+// }
 
 
 // Visa att funktion getTodos är async och inte sync. Synkrona körs efter varandra, i taget
@@ -44,10 +49,8 @@ getTodos();
 console.log("Ett");
 console.log("Två");
 console.log("tre");
-for(let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   console.log(i);
 }
 console.log("fyra");
 console.log("fem");
-
-
